18f4d790524f4d40274c7ee43b44f54a2bd212e2
Alejandro <alejandrogomcap@campus.monlau.com>
2025-01-28 16:44:14 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './resources/js/app.js',  // Asegúrate de que esta ruta sea correcta
+      },
+    },
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: [
+      'resources/js/**/*.jsx',
+      'resources/js/**/*.js',
     ],
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 });
