bd5cf6a9416afe78516ee81904fb0734efb793c4
Alejandro <alejandrogomcap@campus.monlau.com>
2025-01-28 17:05:03 +0100

    hola

--- app/Helpers/vite.php
+++ app/Helpers/vite.php
@@ -3,7 +3,7 @@
 if (!function_exists('vite')) {
     function vite($path)
     {
-        $manifest = json_decode(file_get_contents(public_path('dist/manifest.json')), true);
+        $manifest = json_decode(file_get_contents(public_path('dist/.vite/manifest.json')), true);
         return asset('dist/' . $manifest[$path]['file']);
     }
 }
